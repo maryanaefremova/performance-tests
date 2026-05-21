0c9c16ad7e975a8b95355c90cc8f69e8576c881b
maryanaefremova <maryana.efremova@gmail.com>
2026-05-21 16:14:35 +0300

    Initial commit

--- docker_compose_example.py
+++ docker_compose_example.py
@@ -2,4 +2,4 @@ from redis import Redis
 
 cache = Redis(host="redis", port=6379)
 cache.set("example", 5)
-print(int(cache.get("example")) ** 2)
+print(int(cache.get("example")) ** 2) 
--- docker_example.py
+++ docker_example.py
@@ -1,3 +1,3 @@
-from termcolor import colored
+from termcolor import colored 
 
 print(colored("Привет из контейнера!", "green"))
