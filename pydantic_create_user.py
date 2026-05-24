a2c0f0a8d5c7f87062b69c8d43374b34f86832cc
maryanaefremova <maryana.efremova@gmail.com>
2026-05-24 18:03:46 +0300

    Знакомство с Pydantic. Практика написания Pydantic-моделей

---
+++ pydantic_create_user.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel, Field, EmailStr
+
+
+class UserSchema(BaseModel):
+    """
+    Описание структуры пользователя.
+    """
+    id: str
+    email: EmailStr
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+    phone_number: str = Field(alias="phoneNumber")
+
+
+class CreateUserRequestSchema(BaseModel):
+    """
+    Структура данных для создания нового пользователя.
+    """
+    email: EmailStr
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+    phone_number: str = Field(alias="phoneNumber")
+
+
+class CreateUserResponseSchema (BaseModel):
+    """
+    Описание структуры ответа создания пользователя.
+    """
+    user: UserSchema
