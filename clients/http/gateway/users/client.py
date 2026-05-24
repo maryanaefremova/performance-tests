bbc2a45b1a10a2df49c62edbee02d43bbda61f43
maryanaefremova <maryana.efremova@gmail.com>
2026-05-24 20:44:40 +0300

    Знакомство с Faker

--- clients/http/gateway/users/client.py
+++ clients/http/gateway/users/client.py
@@ -43,17 +43,9 @@ class UsersGatewayHTTPClient(HTTPClient):
         # Инициализируем модель через валидацию JSON строки
         return GetUserResponseSchema.model_validate_json(response.text)
 
-    # Теперь используем pydantic-модель для аннотации
     def create_user(self) -> CreateUserResponseSchema:
-        request = CreateUserRequestSchema(  # Используем pydantic-модель для отправки запроса
-            email=f"user.{time.time()}@example.com",  # Передаем аргументы в формате snake_case вместо camelCase
-            last_name="string",  # Передаем аргументы в формате snake_case вместо camelCase
-            first_name="string",  # Передаем аргументы в формате snake_case вместо camelCase
-            middle_name="string",  # Передаем аргументы в формате snake_case вместо camelCase
-            phone_number="string"  # Передаем аргументы в формате snake_case вместо camelCase
-        )
+        request = CreateUserRequestSchema()
         response = self.create_user_api(request)
-        # Инициализируем модель через валидацию JSON строки
         return CreateUserResponseSchema.model_validate_json(response.text)
 
 
--- clients/http/gateway/users/schema.py
+++ clients/http/gateway/users/schema.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, Field, EmailStr, ConfigDict
+from tools.fakers import fake
 
 
 # Добавили суффикс Schema вместо Dict
@@ -29,11 +30,11 @@ class CreateUserRequestSchema(BaseModel):  # Наследуем от BaseModel �
     """
     model_config = ConfigDict(populate_by_name=True)
 
-    email: EmailStr
-    last_name: str = Field(alias="lastName")  # Использовали alise
-    first_name: str = Field(alias="firstName")  # Использовали alise
-    middle_name: str = Field(alias="middleName")  # Использовали alise
-    phone_number: str = Field(alias="phoneNumber")  # Использовали alise
+    email: EmailStr = Field(default_factory=fake.email)
+    last_name: str = Field(alias="lastName", default_factory=fake.last_name)
+    first_name: str = Field(alias="firstName", default_factory=fake.first_name)
+    middle_name: str = Field(alias="middleName", default_factory=fake.middle_name)
+    phone_number: str = Field(alias="phoneNumber", default_factory=fake.phone_number)
 
 
 # Добавили суффикс Schema вместо Dict
