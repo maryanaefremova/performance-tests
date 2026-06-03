e1327c0229ecf2297da7e157ef969987845bfcb6
maryanaefremova <maryana.efremova@gmail.com>
2026-06-03 16:40:14 +0300

    Практика генерации данных с помощью сидингов. Практика: реализация сидинга для сценария получения информации об операциях

---
+++ seeds/scenarios/existing_user_get_operations.py
@@ -0,0 +1,41 @@
+from seeds.scenario import SeedsScenario
+from seeds.schema.plan import SeedOperationsPlan, SeedsPlan, SeedUsersPlan, SeedAccountsPlan
+
+
+class ExistingUserGetOperationsSeedsScenario(SeedsScenario):
+    """
+    Сценарий сидинга для существующего пользователя, который просматривает информацию об операциях.
+    Создаёт 300 пользователей, открывает кредитный счёт и совершает операции.
+    """
+
+    @property
+    def plan(self) -> SeedsPlan:
+        """
+        План сидинга, который описывает, сколько пользователей нужно создать
+        и какие именно данные для них генерировать.
+        В данном случае создаём 300 пользователей, каждому даём кредитный счёт, 
+        совершаем 5 операций покупки, 1 операцию пополнения счета и 1 операцию снятия наличных.
+        """
+        return SeedsPlan(
+            users=SeedUsersPlan(
+                count=300,
+                credit_card_accounts=SeedAccountsPlan(
+                    count=1,
+                    purchase_operations=SeedOperationsPlan(count=5),
+                    top_up_operations=SeedOperationsPlan(count=1),
+                    cash_withdrawal_operations=SeedOperationsPlan(count=1)
+                )
+            ),
+        )
+
+    @property
+    def scenario(self) -> str:
+        """
+        Название сценария сидинга, которое будет использоваться для сохранения данных.
+        """
+        return "existing_user_get_operations"
+
+
+if __name__ == '__main__':
+    seeds_scenario = ExistingUserGetOperationsSeedsScenario()
+    seeds_scenario.build()
